bfd06aaec760227500926abf4e78600587a39d17
David mochoge <kingkgdave@gmail.com>
2019-03-22 14:23:24 +0300

    add files

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Quote } from './quote'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  quote = [
+    new Quote(1,'Watch Finding Nemo'),
+    new Quote(2,'Buy Cookies'),
+    new Quote(3, 'Get new Phone Case'),
+    new Quote(4, 'Get Dog Food'),
+    new Quote(5, 'Solve math homework'),
+    new Quote(6, 'Plot my world domination plan'),
+]
+
+}
