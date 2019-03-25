7e0eca97b5821cf000cc3ef28f31f2af4e30c3dc
David mochoge <kingkgdave@gmail.com>
2019-03-25 10:56:58 +0300

    add background image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,14 @@ import { Quote } from './quote'
 })
 export class AppComponent {
   quote = [
-    new Quote(1,'Watch Finding Nemo',),
-    new Quote(2,'Buy Cookies'),
-    new Quote(3, 'Get new Phone Case'),
-    new Quote(4, 'Get Dog Food'),
-    new Quote(5, 'Solve math homework'),
-    new Quote(6, 'Plot my world domination plan'),
-];
+    new Quote(1,'Eleanor Roosevelt','“Great minds discuss ideas; average minds discuss events; small minds discuss people.”',new Date(2018,3,14)),
+    new Quote(2,'David Brinkley','“A successful man is one who can lay a firm foundation with the bricks others have thrown at him.”',new Date(2018,3,14)),
+    new Quote(3, '-John F. Kennedy','“Those who dare to fail miserably can achieve greatly.”',new Date(2018,3,14)),
+    new Quote(4, 'Stephen King','“Get busy living or get busy dying.”',new Date(2018,3,14)),
+    new Quote(5, 'Solve math homework','Damn Math',new Date(2018,3,14)),
+    new Quote(6, 'Abraham Lincoln','“I’m a success today because I had a friend who believed in me and I didn’t have the heart to let him down.'
+    ,new Date(2018,3,14)),
+]
 constructor(){  }
 
 }
